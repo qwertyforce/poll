d13885cc094abcf891a4a8efb374979b6f7504d0
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-05-23 18:05:47 +0300

    Create_poll fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import {Switch,Route} from "react-router-dom";
 import CreatePoll from './CreatePoll'
+import Poll from './Poll'
 function App() {
   return (
     <Switch>
       <Route exact path="/">
         <CreatePoll />
       </Route>
+      <Route path="/poll/:id">
+        <Poll/>
+      </Route>  
     </Switch>
   )
 }
