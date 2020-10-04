32b2bed877ba13f4b5e9e74c92eaa8a44a98dd4c
qwertyforce <44163887+qwertyforce@users.noreply.github.com>
2020-10-04 13:07:56 +0300

    it's working now

---
+++ backend/config/config.js
@@ -0,0 +1,8 @@
+export default {
+    recaptcha_site_key: "6LcqV9QUAAAAAEybBVr0FWnUnFQmOVxGoQ_Muhtb",
+    recaptcha_secret_key: "6LcqV9QUAAAAAOA18kbCEWRBhF4g4LjSTaFRVe9P",
+    server_port: 80,
+    front_end_server_url:"http://localhost:3000",
+    front_end_domain: "localhost",
+    mongodb_url:'mongodb://localhost/',
+}
